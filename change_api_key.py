f504d8399ff61d50bb741edbd566a7241a78812b
Aashay <snipernationsim@gmail.com>
2024-03-14 21:45:18 +0530

    Added a credits button and toplevel window, and reformatted code

--- change_api_key.py
+++ change_api_key.py
@@ -8,13 +8,13 @@ primary_color = "#6F42C1"  # Example accent color
 
 def change_api_key(model, entry):
     config = ConfigParser()
-    config.read('config.ini')
+    config.read("config.ini")
     new_api_key = entry.get()
     if model == "gpt":
-        config.set('gpt', 'GPT_3_API_KEY', new_api_key)
+        config.set("gpt", "GPT_3_API_KEY", new_api_key)
     elif model == "gemini-pro":
-        config.set('gemini', 'GEMINI_API_KEY', new_api_key)
-    with open('config.ini', 'w') as configfile:
+        config.set("gemini", "GEMINI_API_KEY", new_api_key)
+    with open("config.ini", "w") as configfile:
         config.write(configfile)
 
 
@@ -23,29 +23,66 @@ def open_api_key_window(font):
     api_key_window.title("Change API Key")
     api_key_window.geometry("300x200")
 
-    gpt_label = Label(api_key_window, text="ChatGPT API Key:", bg=background_color, fg=text_color,
-                      font=font.Font(family="Google Sans"))
+    gpt_label = Label(
+        api_key_window,
+        text="ChatGPT API Key:",
+        bg=background_color,
+        fg=text_color,
+        font=font.Font(family="Google Sans"),
+    )
     gpt_label.pack()
-    gpt_entry = Entry(api_key_window, bg=background_color, fg=text_color, insertbackground=text_color,
-                      font=font.Font(family="Google Sans"))
+    gpt_entry = Entry(
+        api_key_window,
+        bg=background_color,
+        fg=text_color,
+        insertbackground=text_color,
+        font=font.Font(family="Google Sans"),
+    )
     gpt_entry.pack()
-    gpt_button = Button(api_key_window, text="Change ChatGPT API Key", command=lambda: change_api_key("gpt", gpt_entry),
-                        bg=background_color, fg=text_color, font=font.Font(family="Google Sans", size=10))
+    gpt_button = Button(
+        api_key_window,
+        text="Change ChatGPT API Key",
+        command=lambda: change_api_key("gpt", gpt_entry),
+        bg=background_color,
+        fg=text_color,
+        font=font.Font(family="Google Sans", size=10),
+    )
     gpt_button.pack(padx=10, pady=10)
 
-    gemini_label = Label(api_key_window, text="Gemini API Key:", bg=background_color, fg=text_color,
-                         font=font.Font(family="Google Sans"))
+    gemini_label = Label(
+        api_key_window,
+        text="Gemini API Key:",
+        bg=background_color,
+        fg=text_color,
+        font=font.Font(family="Google Sans"),
+    )
     gemini_label.pack()
-    gemini_entry = Entry(api_key_window, bg=background_color, fg=text_color, insertbackground=text_color,
-                         font=font.Font(family="Google Sans"))
+    gemini_entry = Entry(
+        api_key_window,
+        bg=background_color,
+        fg=text_color,
+        insertbackground=text_color,
+        font=font.Font(family="Google Sans"),
+    )
     gemini_entry.pack()
-    gemini_button = Button(api_key_window, text="Change Gemini API Key",
-                           command=lambda: change_api_key("gemini-pro", gemini_entry), bg=background_color,
-                           fg=text_color, font=font.Font(family="Google Sans", size=10))
+    gemini_button = Button(
+        api_key_window,
+        text="Change Gemini API Key",
+        command=lambda: change_api_key("gemini-pro", gemini_entry),
+        bg=background_color,
+        fg=text_color,
+        font=font.Font(family="Google Sans", size=10),
+    )
     gemini_button.pack(padx=10, pady=10)
 
 
 def create_change_api_key_button(root, open_api_key_window, font):
-    change_api_key_button = Button(root, text="Change API Key", command=lambda: open_api_key_window(font),
-                                   bg=background_color, fg=text_color, font=font.Font(family="Google Sans"))
+    change_api_key_button = Button(
+        root,
+        text="Change API Key",
+        command=lambda: open_api_key_window(font),
+        bg=background_color,
+        fg=text_color,
+        font=font.Font(family="Google Sans"),
+    )
     return change_api_key_button
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 # Import libraries
 import tkinter
-from tkinter import Tk, Entry, Text, Button, END, PhotoImage, font, Frame, Radiobutton, StringVar
+from tkinter import (
+    Tk,
+    Entry,
+    Text,
+    Button,
+    END,
+    PhotoImage,
+    font,
+    Frame,
+    Radiobutton,
+    StringVar,
+)
 
 from change_api_key import open_api_key_window, create_change_api_key_button
 from get_gemini_pro import get_gemini_pro_response
 from get_gpt_3 import get_gpt3_response
+from credits import show_credits_window
 
 background_color = "#212121"
 text_color = "white"
@@ -13,20 +25,32 @@ primary_color = "#6F42C1"  # Example accent color
 # Define the chatbot window
 root = Tk()
 root.title("AI Hub")
-root.geometry("1280x720")
+root.geometry("1280x730")
 root.configure(bg=background_color)
 # Create a frame for the sidebar
-sidebar = Frame(root, width=260, height=605, bg="#36373B")  # Adjust width and height as needed
+sidebar = Frame(
+    root, width=260, height=605, bg="#36373B"
+)  # Adjust width and height as needed
 sidebar.grid(row=0, column=2, padx=10, pady=10)
 
 ai_options = ["ChatGPT", "Gemini"]  # List of AI options
-ai_models = {"ChatGPT": "gpt", "Gemini": "gemini-pro"}  # Model IDs for each option (replace with actual IDs)
+ai_models = {
+    "ChatGPT": "gpt",
+    "Gemini": "gemini-pro",
+}  # Model IDs for each option (replace with actual IDs)
 current_ai = "gemini-pro"  # Default AI model (should match a key in ai_models)
 # Function to change the AI model
 
 # Create a text box to display conversation history
-chat_history = Text(root, height=30, width=97, font=font.Font(family="Google Sans"), bg=background_color, fg=text_color,
-                    state="disabled")
+chat_history = Text(
+    root,
+    height=30,
+    width=97,
+    font=font.Font(family="Google Sans"),
+    bg=background_color,
+    fg=text_color,
+    state="disabled",
+)
 chat_history.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 chat_history.config(state="normal")
 chat_history.insert(END, "Welcome! How can I help you today?\n")
@@ -37,8 +61,9 @@ def change_ai(selected_ai):
     global current_ai  # Access global variable for current AI
     current_ai = ai_models[selected_ai]
     chat_history.config(state="normal")
-    chat_history.insert(END,
-                        f"Switched to AI: {current_ai}\n")  # Update for debugging (replace with your logic to handle
+    chat_history.insert(
+        END, f"Switched to AI: {current_ai}\n"
+    )  # Update for debugging (replace with your logic to handle
     # AI change)
     chat_history.config(state="disabled")
 
@@ -47,18 +72,37 @@ def change_ai(selected_ai):
 selected_ai_var = StringVar(value=current_ai)  # Pre-select default AI
 row_index = 0  # Track the row index for radio buttons
 for option in ai_options:
-    radio_button = Radiobutton(sidebar, text=option, variable=selected_ai_var, value=ai_models[option],
-                               command=lambda ai=option: change_ai(ai), bg=background_color, fg=text_color,
-                               selectcolor=primary_color, activebackground=background_color,
-                               activeforeground=text_color, font=font.Font(family="Google Sans"), width=25, anchor="n")
-    radio_button.grid(row=row_index, padx=5, pady=5)  # Use pack for radio buttons within the frame
+    radio_button = Radiobutton(
+        sidebar,
+        text=option,
+        variable=selected_ai_var,
+        value=ai_models[option],
+        command=lambda ai=option: change_ai(ai),
+        bg=background_color,
+        fg=text_color,
+        selectcolor=primary_color,
+        activebackground=background_color,
+        activeforeground=text_color,
+        font=font.Font(family="Google Sans"),
+        width=25,
+        anchor="n",
+    )
+    radio_button.grid(
+        row=row_index, padx=5, pady=5
+    )  # Use pack for radio buttons within the frame
     row_index += 1  # Increment the row index for the next radio button
 # ... rest of your code for sidebar content ...
 
 
 # Create an entry field for user input
-user_input = Entry(root, width=93, font=font.Font(family="Google Sans"), bg=background_color, fg="gray",
-                   insertbackground=text_color)
+user_input = Entry(
+    root,
+    width=93,
+    font=font.Font(family="Google Sans"),
+    bg=background_color,
+    fg="gray",
+    insertbackground=text_color,
+)
 user_input.grid(row=1, column=0, padx=10, pady=10)
 
 
@@ -88,15 +132,19 @@ def send_message(current_ai):
     chat_history.config(state="normal")
     chat_history.insert(END, f"You: {user_message}\n")
     chat_history.config(state="disabled")
-    user_input.delete(0, 'end')  # Clear user input field
+    user_input.delete(0, "end")  # Clear user input field
 
     if current_ai == "gemini-pro":
-        gemini_response = get_gemini_pro_response(user_message)  # Call the function to get Gemini response
+        gemini_response = get_gemini_pro_response(
+            user_message
+        )  # Call the function to get Gemini response
         chat_history.config(state="normal")
         chat_history.insert(END, f"Bot (Gemini): {gemini_response}\n")
         chat_history.config(state="disabled")
     elif current_ai == "gpt":
-        gpt3_response = get_gpt3_response(user_message)  # Call the function to get GPT-3 response
+        gpt3_response = get_gpt3_response(
+            user_message
+        )  # Call the function to get GPT-3 response
         chat_history.config(state="normal")
         chat_history.insert(END, f"Bot (GPT-3): {gpt3_response}\n")
         chat_history.config(state="disabled")
@@ -110,10 +158,26 @@ def send_message(current_ai):
 # Create a button to trigger the command window to change the API key
 change_api_key_button = create_change_api_key_button(root, open_api_key_window, font)
 change_api_key_button.grid(row=1, column=2, padx=10, pady=10)
+
+# Create a button to show the credits window
+credits_button = Button(
+    root,
+    text="Credits",
+    command=show_credits_window,
+    bg=background_color,
+    fg=text_color,
+    font=font.Font(family="Google Sans"),
+)
+credits_button.grid(row=2, column=2, padx=50, pady=10)
 # Create a send button to trigger processing
 send_icon = PhotoImage(file="send_icon.png")
-send_button = Button(root, image=send_icon, command=lambda: send_message(current_ai), borderwidth=0,
-                     bg=background_color)
+send_button = Button(
+    root,
+    image=send_icon,
+    command=lambda: send_message(current_ai),
+    borderwidth=0,
+    bg=background_color,
+)
 send_button.grid(row=1, column=1, padx=10, pady=10)
 
 # Start the main event loop
